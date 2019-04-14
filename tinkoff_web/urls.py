5e49f0b308a522236fef2a5580a6e06de30ff9b2
subelta <40162440+subelta@users.noreply.github.com>
2019-04-14 19:37:54 +0300

    added django-cors-headers

--- tinkoff_web/urls.py
+++ tinkoff_web/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     # when client request http://127.0.0.1:8000/index/, the project will find the mapped process method in my_hello_world app's urls.py file.
-    path('index/', include('helloworld.urls'))
+    path('', include('helloworld.urls'))
 ]
