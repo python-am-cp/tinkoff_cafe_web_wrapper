68a8adbcfe022c56f92ad59994e03d2c46a41ced
subelta <40162440+subelta@users.noreply.github.com>
2019-04-17 20:15:08 +0300

    Добавил обработку пользовательских данных, объект пользователя, уникальные имена папок, отправку на соответствующий email

---
+++ helloworld/classes.py
@@ -0,0 +1,8 @@
+
+class User:
+    def __init__(self, name='', email='', resName='', resDesc='', path=''):
+        self.name = name
+        self.email = email
+        self.resName = resName
+        self.resDesc = resDesc
+        self.path = path
--- helloworld/views.py
+++ helloworld/views.py
@@ -1,17 +1,19 @@
 from django.shortcuts import render
 import os
-import threading
-from django.core.mail import EmailMessage
-from. import validation
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 import time
 import shutil
+import random
+from helloworld.classes import User
+from django.core.mail import EmailMessage
+from. import validation
 
 
 INPUT_DIR = "X:/Docs/Pycharm Projects/tinkoff_cafe_web_wrapper/helloworld/" \
             "input_user/"
 doLogic = False
+curr_user = User()
 
 
 def directComputationReq(request):
@@ -25,67 +27,76 @@ def directComputationReq(request):
     f.close()
     em = EmailMessage(subject='Result', body='Ваш результат', to=['atomicmaize@gmail.com'])
     em.attach_file(r"X:/Docs/Pycharm Projects/tinkoff_cafe_web_wrapper/helloworld/"
-             "input_user/user1/result.txt")
+                   "input_user/user1/result.txt")
     em.send()
     return HttpResponse('\n'.join([line1, line2]))
 
 
 def doMainLogic():
     global doLogic
+    global curr_user
     while True:
         if doLogic:
-            validation.valid(INPUT_DIR + 'user1/')
-            f = open(INPUT_DIR + 'user1/' + "result.txt", "r")
+            # validation.valid(curr_user.path)
+            with open(curr_user.path + 'result.txt', 'w+') as dest:
+                    dest.write('134423 \n 4354')
+            f = open(curr_user.path + "result.txt", "r")
             while True:
                 line1 = f.readline()
                 line2 = f.readline()
                 line3 = f.readline()
                 if not line3: break
             f.close()
-            em = EmailMessage(subject='Result', body='Ваш результат', to=['atomicmaize@gmail.com'])
-            em.attach_file(r"X:/Docs/Pycharm Projects/tinkoff_cafe_web_wrapper/helloworld/"
-                     "input_user/user1/result.txt")
-            em.send()
-            shutil.rmtree(INPUT_DIR + 'user1/', ignore_errors=True)
+            subj = 'Результат' + ' ' + curr_user.resName
+            mailContent = 'Здравствуйте,' + ' ' + curr_user.name + ', ' + 'результат ' \
+                          'расчетов по метрике составил: ' + '\n\n' + str(line1) + \
+                          str(line2) + '\n\n' + 'Описание вашего исследования:' + ' '\
+                          + curr_user.resDesc
+
+            msg = EmailMessage(subject=subj, body=mailContent, to=[curr_user.email])
+            # em.attach_file(curr_user.path + 'result.txt')
+            msg.send()
+            shutil.rmtree(curr_user.path, ignore_errors=True)
             doLogic = False
+            curr_user = User()
         time.sleep(5)
 
 
 
-def handleUploaded(reqFiles):
+def handleUserData(data):
+    userName = data.get('user-name')
+    email = data.get('user-email')
+    resName = data.get('research-name')
+    resDesc = data.get('research-desc')
+    userFolder = email + str(random.randint(10000, 99999)) + '/'
+    userPath = INPUT_DIR + userFolder
+
+    ThisUser = User(userName, email, resName, resDesc, userPath)
+    return ThisUser
+
+
+def handleUserFiles(reqFiles, user):
+    if not os.path.exists(user.path):
+        os.makedirs(user.path)
     for key, value in reqFiles.items():
-        name = reqFiles[key].name
-        if not os.path.exists(INPUT_DIR + 'user1/'):
-            os.makedirs(INPUT_DIR + 'user1/')
-        with open(INPUT_DIR + 'user1/' + name, 'wb+') as destination:
+        fileName = reqFiles[key].name
+        with open(user.path + fileName, 'wb+') as destination:
             for chunk in reqFiles[key].chunks():
                 destination.write(chunk)
 
 
-
 @csrf_exempt
-def uploadFiles(request):
+def receiveForm(request):
     global doLogic
+    global curr_user
     if request.method == 'POST':
-        handleUploaded(request.FILES)
+        userData = request.POST.copy()
+        curr_user = handleUserData(userData)
+        handleUserFiles(request.FILES, curr_user)
         doLogic = True
         return HttpResponse("Ваши файлы сохранены")
     return HttpResponse("Метод должен быть POST")
 
 
-# validation.valid('helloworld/input_user/')
-# def printit():
-#     threading.Timer(10.0, printit).start()
-#     if not os.listdir('helloworld/input_user/'):
-#         print('COOL!')
-#     else:
-#         print('FUCK!')
-#
-# printit()
-
-
 def index_page(request):
-    # Get the index template file absolute path.
-    # index_file_path = PROJECT_PATH + '/pages/home.html'
-    # Return the index file to client.
     return render(request, 'index.html')
