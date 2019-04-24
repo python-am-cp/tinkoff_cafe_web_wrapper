a36115f4c9dcc371f36c1715deb14ee4d36d9ca1
subelta <awaspecial10@gmail.com>
2019-04-24 13:04:27 +0300

    сделал скрипт демон, эксперементирую

---
+++ helloworld/task_queue.py
@@ -0,0 +1,58 @@
+import sys
+import time
+import shutil
+from django.core.mail import EmailMessage
+
+from .daemon import Daemon
+from .views import taskQueue
+from. import cross_validation
+
+
+class TaskDaemon(Daemon):
+    def run(self):
+        while True:
+            if not taskQueue.empty():
+                current = taskQueue.get()
+                calculate_send_clear(current)
+            time.sleep(5)
+
+
+def calculate_send_clear(curr_res):
+    cross_validation.validate(curr_res.path)
+    # with open(curr_res.path + 'result.txt', 'w+') as dest:
+    #         dest.write('134423 \n 435423')
+    f = open(curr_res.path + "result.txt", "r")
+    while True:
+        line1 = f.readline()
+        line2 = f.readline()
+        line3 = f.readline()
+        if not line3:
+            break
+    f.close()
+    subj = 'Результат' + ' ' + curr_res.resName
+    mail_content = 'Здравствуйте,' + ' ' + curr_res.name + ', ' + 'результат ' \
+                   'расчетов по метрике составил: ' + '\n\n' + str(line1) + \
+                   str(line2) + '\n\n' + 'Описание вашего исследования:' + ' ' \
+                   + curr_res.resDesc
+
+    msg = EmailMessage(subject=subj, body=mail_content, to=[curr_res.email])
+    msg.send()
+    shutil.rmtree(curr_res.path, ignore_errors=True)
+
+
+if __name__ == "__main__":
+    daemon = TaskDaemon('/tmp/daemon-example.pid')
+    if len(sys.argv) == 2:
+        if 'start' == sys.argv[1]:
+            daemon.start()
+        elif 'stop' == sys.argv[1]:
+            daemon.stop()
+        elif 'restart' == sys.argv[1]:
+            daemon.restart()
+        else:
+            print("Unknown command")
+            sys.exit(2)
+        sys.exit(0)
+    else:
+        print("usage: %s start|stop|restart" % sys.argv[0])
+        sys.exit(2)
--- helloworld/views.py
+++ helloworld/views.py
@@ -16,13 +16,13 @@ from. import cross_validation
 taskQueue = queue.Queue(maxsize=6)
 
 
-def handle_queue():
-    global taskQueue
-    while True:
-        if not taskQueue.empty():
-            current = taskQueue.get()
-            calculate_send_clear(current)
-        time.sleep(5)
+# def handle_queue():
+#     global taskQueue
+#     while True:
+#         if not taskQueue.empty():
+#             current = taskQueue.get()
+#             calculate_send_clear(current)
+#         time.sleep(5)
 
 
 @csrf_exempt
@@ -61,33 +61,33 @@ def handle_user_files(req_files, current_res):
                 destination.write(chunk)
 
 
-def calculate_send_clear(curr_res):
-    cross_validation.validate(curr_res.path)
-    # with open(curr_res.path + 'result.txt', 'w+') as dest:
-    #         dest.write('134423 \n 435423')
-    f = open(curr_res.path + "result.txt", "r")
-    while True:
-        line1 = f.readline()
-        line2 = f.readline()
-        line3 = f.readline()
-        if not line3:
-            break
-    f.close()
-    subj = 'Результат' + ' ' + curr_res.resName
-    mail_content = 'Здравствуйте,' + ' ' + curr_res.name + ', ' + 'результат ' \
-                   'расчетов по метрике составил: ' + '\n\n' + str(line1) + \
-                   str(line2) + '\n\n' + 'Описание вашего исследования:' + ' ' \
-                   + curr_res.resDesc
-
-    msg = EmailMessage(subject=subj, body=mail_content, to=[curr_res.email])
-    msg.send()
-    shutil.rmtree(curr_res.path, ignore_errors=True)
+# def calculate_send_clear(curr_res):
+#     cross_validation.validate(curr_res.path)
+#     # with open(curr_res.path + 'result.txt', 'w+') as dest:
+#     #         dest.write('134423 \n 435423')
+#     f = open(curr_res.path + "result.txt", "r")
+#     while True:
+#         line1 = f.readline()
+#         line2 = f.readline()
+#         line3 = f.readline()
+#         if not line3:
+#             break
+#     f.close()
+#     subj = 'Результат' + ' ' + curr_res.resName
+#     mail_content = 'Здравствуйте,' + ' ' + curr_res.name + ', ' + 'результат ' \
+#                    'расчетов по метрике составил: ' + '\n\n' + str(line1) + \
+#                    str(line2) + '\n\n' + 'Описание вашего исследования:' + ' ' \
+#                    + curr_res.resDesc
+#
+#     msg = EmailMessage(subject=subj, body=mail_content, to=[curr_res.email])
+#     msg.send()
+#     shutil.rmtree(curr_res.path, ignore_errors=True)
 
 
 def index_page(request):
     return render(request, 'index.html')
 
-#
+
 # logicThread = threading.Thread(target=handle_queue, args=[])
 # logicThread.setDaemon(False)
 # logicThread.start()
