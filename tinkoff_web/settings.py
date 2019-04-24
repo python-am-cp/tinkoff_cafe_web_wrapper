9ccd6c0a32e0f88fba0cdaa9eef81a35f707c8f0
subelta <awaspecial10@gmail.com>
2019-04-24 20:54:25 +0300

    обновил api ключ, исправил путь к tmp

--- tinkoff_web/settings.py
+++ tinkoff_web/settings.py
@@ -132,7 +132,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 PROJECT_ROOT = os.path.join(os.path.abspath("manage.py")[:-9])
 # INPUT_DIR = os.path.join(PROJECT_ROOT, "helloworld/input_user/")
-INPUT_DIR = '/tmp'
+INPUT_DIR = 'tmp/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/staticfiles/'
