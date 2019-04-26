7a84c5250f9c922a6b8d2cb519b9a052edbdcd38
subelta <awaspecial10@gmail.com>
2019-04-26 22:22:54 +0300

    возвращаю обратно

--- helloworld/task.py
+++ helloworld/task.py
@@ -1,13 +1,12 @@
 import shutil
 import os
-from django.core.mail import EmailMessage
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django.conf.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tinkoff_web.settings')
 from django.core.mail import send_mail
 # from tinkoff_web import settings
-import sendgrid
+
 from django.conf import settings
-from django.template.loader import get_template
+
 from sendgrid.helpers.mail import *
 from helloworld import cross_validation
 
@@ -37,18 +36,6 @@ def calculate_send_clear(curr_res):
                    'расчетов по метрике составил: ' + '\n\n'
 
     print("hello")
-    # sg = sendgrid.SendGridAPIClient(api_key=os.environ.get('SENDGRID_API_KEY'))
-    # from_email1 = Email("app131467002@heroku.com")
-    # subject1 = "Hello World from the SendGrid Python Library!"
-    # to_email1 = Email(curr_res.email)
-    # content1 = Content("text/plain", "Hello, Email!")
-    # mail1 = Mail(from_email1, subject1, to_email1, content1)
-    # response = sg.client.mail1.send.post(request_body=mail1.get())
-    # print(response.status_code)
-    # print(response.body)
-    # print(response.headers)
-    # msg = EmailMessage(subject=subj, body=mail_content, to=[curr_res.email])
-    # msg.send()
     print(curr_res.email)
     snd = send_mail(subj,
                     mail_content,
